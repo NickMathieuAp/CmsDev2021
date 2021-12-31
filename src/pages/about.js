5116cf6a71ae6813c7fd3ea46fd83972ee1dc19a
Nick Mathieu <nick.mathieu@student.ap.be>
2021-12-31 13:39:26 +0100

    eindpunt react in Gatsby, start opdracht

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Us">
+        <p>The vintage guitar site is a showroom where vintage guitars and their history are cataloged.</p>
+    </Layout>
+  )
+}
+
+
+export default AboutPage
